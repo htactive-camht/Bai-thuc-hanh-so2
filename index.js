6d0e310404543ee07efeaf01b1a04b15baeb5299
htcative-camht <camht@htactive.com>
2020-06-05 17:51:26 +0700

    commit final

--- index.js
+++ index.js
@@ -1,65 +1,49 @@
-// // Tạo button xóa và thêm nó vào trong cá thẻ li
-// var myNodelist = document.getElementsByTagName("LI");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
-
-// // Khi button xóa được click thì ẩn phần tử li chứa nó
-// var close = document.getElementsByClassName("close");
-// var i;
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function () {
-//     var div = this.parentElement;
-//     div.style.display = "none";
-//   };
-// }
-
-// // Thêm class checked vào thẻ li nào được click
-// var list = document.querySelector("ul");
-// list.addEventListener(
-//   "click",
-//   function (ev) {
-//     if (ev.target.tagName === "LI") {
-//       ev.target.classList.toggle("checked");
-//     }
-//   },
-//   false
-// );
-
-// Thêm công việc mới khi click vào button "thêm"
-function newElement() {
-  // tạo node li
+function addList(todoId, inputId, span1, spanOrther) {
   var li = document.createElement("li");
-  // lấy giá trị người dùng nhập
-  var inputValue = document.getElementById("myInput").value;
-  // tạo đối tượng text node
-  var t = document.createTextNode(inputValue);
-  // gán text node cho thẻ li vừa tạo
-  li.appendChild(t);
-  // kiểm tra nếu text node rỗng thì cảnh báo, nếu không thì thêm vào trong thẻ ul
-  if (inputValue === "") {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  // xóa giá trị thẻ input
-  document.getElementById("myInput").value = "";
-  // thêm button xóa vào thẻ li mới tạo
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  var input = document.getElementById(inputId).value;
+  li.innerHTML = input;
+  var span = document.createElement("span");
   span.className = "close";
-  span.appendChild(txt);
+  span.innerHTML = "x";
   li.appendChild(span);
-  // gán lại sự kiện cho các button xóa
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
-      div.style.display = "none";
-    };
+  document.getElementById(todoId).appendChild(li);
+  document.getElementById(inputId).style.display = "none";
+  document.getElementById(span1).style.display = "none";
+  document.getElementById(spanOrther).style.display = "block";
+}
+
+//  Hiện thị list
+function showInputAdd(task, span) {
+  document.getElementById(task).style.display = "block";
+  document.getElementById(span).style.display = "block";
+}
+
+function evenListener(idList, Model, showTask) {
+  var bien = document.getElementById(idList);
+  function chose(e) {
+    var nameTask = e.target;
+    document.getElementById(Model).style.display = "block";
+    document.getElementById(showTask).innerHTML = nameTask.innerHTML;
   }
+  bien.addEventListener("click", chose, false);
+}
+
+function cancel(idModel) {
+  var modal = (document.getElementById(idModel).style.display = "none");
+}
+
+function showModal(listId, taskId, desc) {
+  document.getElementById("id01").style.display = "block";
+  var value = document.getElementById(`${listId}-${taskId}`).innerHTML;
+  document.getElementById("nameTask").innerHTML = value;
+  document.getElementById("getDescription").innerHTML = desc;
+}
+
+function modelTop(idModel) {
+  let modal = (document.getElementById(idModel).style.display = "block");
+}
+
+function deleteTask(listId, taskId) {
+  const a = document.getElementById(`${listId}-${taskId}`).parentElement;
+  a.remove();
 }
